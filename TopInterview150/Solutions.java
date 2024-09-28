8fa2c2c40dd2d67c63a7ec8f6475df7a1bda4f10
Yujia <0404sola@gmail.com>
2024-09-28 11:02:08 +1000

    Practice on Sept 28

--- TopInterview150/Solutions.java
+++ TopInterview150/Solutions.java
@@ -1,5 +1,8 @@
 package TopInterview150;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solutions {
   // 88. Merge Sorted Aarry
   public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -41,5 +44,105 @@ class Solutions {
     }
 
     return nums.length - removeNum;
+  }
+
+  // 26. Remove Duplicates from Sorted Array
+  public int removeDuplicates1(int[] nums) {
+    if (nums.length == 0 || nums.length == 1) {
+      return nums.length;
+    }
+
+    int uniqueNum = 1;
+    int emptyNum = 0;
+    int i = 1;
+    while (i < nums.length) {
+      if (i == nums.length - emptyNum)
+        break;
+
+      if (nums[i] == nums[i - 1]) {
+        for (int j = i; j < nums.length; j++) {
+          nums[j - 1] = nums[j];
+        }
+        nums[nums.length - 1] = 0;
+        emptyNum++;
+      } else {
+        uniqueNum++;
+        i++;
+      }
+    }
+    return uniqueNum;
+  }
+
+  // 26. Remove Duplicates from Sorted Array (better solution)
+  public int removeDuplicates(int[] nums) {
+    if (nums.length <= 1)
+      return nums.length;
+
+    int uniqueNum = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[uniqueNum - 1]) {
+        nums[uniqueNum] = nums[i];
+        uniqueNum++;
+      }
+    }
+    return uniqueNum;
+  }
+
+  // 80. Remove Duplicates from Sorted Array II
+  public int removeDuplicates2(int[] nums) {
+    if (nums.length <= 2)
+      return nums.length;
+
+    int uniqueNum = 2;
+    for (int i = 2; i < nums.length; i++) {
+      if (nums[i] != nums[uniqueNum - 2]) {
+        nums[uniqueNum] = nums[i];
+        uniqueNum++;
+      }
+    }
+    return uniqueNum;
+  }
+
+  // 169. Majority Element
+  public int majorityElement(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i : nums) {
+      if (!map.keySet().contains(i))
+        map.put(i, 1);
+      else
+        map.put(i, map.get(i) + 1);
+    }
+
+    for (int key : map.keySet()) {
+      if (map.get(key) > nums.length / 2)
+        return key;
+    }
+
+    return -1;
+  }
+
+  // 189. Rotate Array
+  public void rotate(int[] nums, int k) {
+    while (k >= nums.length) {
+      k -= nums.length;
+    }
+
+    int[] part1 = new int[nums.length - k];
+    int[] part2 = new int[k];
+    for (int i = 0; i < nums.length; i++) {
+      if (i < nums.length - k) {
+        part1[i] = nums[i];
+      } else {
+        part2[i - nums.length + k] = nums[i];
+      }
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (i < k) {
+        nums[i] = part2[i];
+      } else {
+        nums[i] = part1[i - k];
+      }
+    }
   }
 }
