2540b2dbf8c6b64e4a7eb8f27d2881170bb70e14
Yujia <0404sola@gmail.com>
2024-10-04 10:54:39 +1000

    Practice on Oct 4th

--- TopInterview150/Hashmap.java
+++ TopInterview150/Hashmap.java
@@ -1,5 +1,8 @@
 package TopInterview150;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hashmap {
   // 383. Ransom Note
   public boolean canConstruct(String ransomNote, String magazine) {
@@ -14,6 +17,24 @@ public class Hashmap {
       counter[c - 'a']--;
     }
 
+    return true;
+  }
+
+  // 205. Isomorphic Strings
+  public boolean isIsomorphic(String s, String t) {
+    Map<Character, Character> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c1 = s.charAt(i);
+      char c2 = t.charAt(i);
+      if (!map.keySet().contains(c1)) {
+        if (map.values().contains(c2))
+          return false;
+        map.put(c1, c2);
+      } else {
+        if (map.get(c1) != c2)
+          return false;
+      }
+    }
     return true;
   }
 }
