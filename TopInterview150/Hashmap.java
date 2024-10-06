8a3f043a4a2abba88c43cc7cc1aba5023d1a0373
Yujia <0404sola@gmail.com>
2024-10-06 21:46:17 +1100

    Practice on Oct 6th

--- TopInterview150/Hashmap.java
+++ TopInterview150/Hashmap.java
@@ -37,4 +37,26 @@ public class Hashmap {
     }
     return true;
   }
+
+  // 290. Word Pattern
+  public boolean wordPattern(String pattern, String s) {
+    Map<Character, String> map = new HashMap<>();
+    String[] words = s.split(" ");
+
+    if (words.length != pattern.length())
+      return false;
+    for (int i = 0; i < pattern.length(); i++) {
+      char c = pattern.toCharArray()[i];
+      if (map.keySet().contains(c)) {
+        if (!map.get(c).equals(words[i]))
+          return false;
+      } else {
+        if (map.values().contains(words[i]))
+          return false;
+        map.put(c, words[i]);
+      }
+    }
+
+    return true;
+  }
 }
