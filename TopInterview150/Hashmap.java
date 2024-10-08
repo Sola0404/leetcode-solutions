5ccd464e7c4ca060c317d5d7aac1e41b40d229b1
Yujia <0404sola@gmail.com>
2024-10-08 20:29:38 +1100

    Practice on 8th Oct

--- TopInterview150/Hashmap.java
+++ TopInterview150/Hashmap.java
@@ -99,4 +99,17 @@ public class Hashmap {
 
     return new ArrayList<>(map.values());
   }
+
+  // 1. Two Sum
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return new int[] { map.get(complement), i };
+      }
+      map.put(nums[i], i);
+    }
+    return new int[] {};
+  }
 }
