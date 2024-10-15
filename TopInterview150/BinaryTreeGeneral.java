0b6e3e46d9ae57b8aba2ea55a68c0625fd65606f
Yujia <0404sola@gmail.com>
2024-10-15 18:11:27 +1100

    Practice on 15th Oct

--- TopInterview150/BinaryTreeGeneral.java
+++ TopInterview150/BinaryTreeGeneral.java
@@ -31,4 +31,13 @@ public class BinaryTreeGeneral {
 
     return 1 + Math.max(leftDepth, rightDepth);
   }
+
+  // 100. Same Tree
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null)
+      return true;
+    if (p == null || q == null || p.val != q.val)
+      return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
 }
