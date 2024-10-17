9cc415299977b4560fdb6e7e3927f0267752d6e8
Yujia <0404sola@gmail.com>
2024-10-17 18:42:58 +1100

    practice on 17th Oct

--- TopInterview150/BinaryTreeGeneral.java
+++ TopInterview150/BinaryTreeGeneral.java
@@ -40,4 +40,19 @@ public class BinaryTreeGeneral {
       return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
+
+  // 226. Invert Binary Tree
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null)
+      return null;
+
+    TreeNode temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
 }
