e45ba3bd1bc9c9d53dea39e2f77d475e27cf3cf3
Yujia <0404sola@gmail.com>
2024-10-22 20:58:07 +1100

    Practice on 22 Oct

--- TopInterview150/BinaryTreeGeneral.java
+++ TopInterview150/BinaryTreeGeneral.java
@@ -55,4 +55,17 @@ public class BinaryTreeGeneral {
 
     return root;
   }
+
+  // 101. Symmetric Tree
+  public boolean isSymmetric(TreeNode root) {
+    return isMirror(root.left, root.right);
+  }
+
+  private boolean isMirror(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null)
+      return true;
+    if (t1 == null || t2 == null)
+      return false;
+    return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+  }
 }
